b0cd048d920cce404a51788b95bd8ef14ab8e4d2
creativemario <thomasmather7@hotmail.co.uk>
2015-07-27 23:27:29 +0100

    Adding XML Parsing

--- TFL for London/src/main/java/uk/co/reformtheempire/tlfforlondon/main/TFLForLondonStandalone.java
+++ TFL for London/src/main/java/uk/co/reformtheempire/tlfforlondon/main/TFLForLondonStandalone.java
@@ -1,5 +1,6 @@
 package uk.co.reformtheempire.tlfforlondon.main;
 
+import uk.co.reformtheempire.tlfforlondon.reader.Parser;
 import ch.mather.quicklogger.impl.Logger;
 import ch.mather.quicklogger.impl.LoggerFactory;
 
@@ -8,6 +9,9 @@ public class TFLForLondonStandalone {
 	public static void main(String args[]){
 		TFLForLondonStandalone main = new TFLForLondonStandalone();
 		main.run();
+		
+		Parser parser = new Parser();
+		parser.parse();
 	}
 	
 	public void run(){
