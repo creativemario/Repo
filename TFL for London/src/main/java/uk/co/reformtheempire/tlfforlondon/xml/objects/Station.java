e4dd2ede35fd448c5335c7c086eca8b0f8bdec44
creativemario <thomasmather7@hotmail.co.uk>
2015-07-27 23:33:41 +0100

    Fixing names in Staion Object

--- TFL for London/src/main/java/uk/co/reformtheempire/tlfforlondon/xml/objects/Station.java
+++ TFL for London/src/main/java/uk/co/reformtheempire/tlfforlondon/xml/objects/Station.java
@@ -2,99 +2,86 @@ package uk.co.reformtheempire.tlfforlondon.xml.objects;
 
 public class Station {
 
-	private int StationIdentity;
-
-	private String StationName;
-
-	private long StationTerminalName;
-
-	private float StationLatitude;
-
-	private float StationLongitude;
-
-	private boolean StationInstalled;
-
-	private boolean StationLocked;
-
-	private long StationInstallDate;
-
-	private long StationRemovalDate;
-
-	private boolean StationTemporaryStatus;
-
-	private int StationNumberOfBikes;
-
-	private int StationNumberOfEmptyDocks;
-
-	private int StationNumberOfDocks;
+	private int id;
+	private String name;
+	private long terminalName;
+	private float latitude;
+	private float longitude;
+	private boolean installed;
+	private boolean locked;
+	private long installDate;
+	private long removalDate;
+	private boolean temporaryStatus;
+	private int numberOfBikes;
+	private int numberOfEmptyDocks;
+	private int numberOfDocks;
 
 	public Station(int stationIdentity, String stationName, long stationTerminalName, float stationLatitude, float stationLongitude, boolean stationInstalled, boolean stationLocked, long stationInstallDate, long stationRemovalDate, boolean stationTemporaryStatus, int stationNumberOfBikes, int stationNumberOfEmptyDocks, int stationNumberOfDocks) {
-		;
-		StationIdentity = stationIdentity;
-		StationName = stationName;
-		StationTerminalName = stationTerminalName;
-		StationLatitude = stationLatitude;
-		StationLongitude = stationLongitude;
-		StationInstalled = stationInstalled;
-		StationLocked = stationLocked;
-		StationInstallDate = stationInstallDate;
-		StationRemovalDate = stationRemovalDate;
-		StationTemporaryStatus = stationTemporaryStatus;
-		StationNumberOfBikes = stationNumberOfBikes;
-		StationNumberOfEmptyDocks = stationNumberOfEmptyDocks;
-		StationNumberOfDocks = stationNumberOfDocks;
+		id = stationIdentity;
+		name = stationName;
+		terminalName = stationTerminalName;
+		latitude = stationLatitude;
+		longitude = stationLongitude;
+		installed = stationInstalled;
+		locked = stationLocked;
+		installDate = stationInstallDate;
+		removalDate = stationRemovalDate;
+		temporaryStatus = stationTemporaryStatus;
+		numberOfBikes = stationNumberOfBikes;
+		numberOfEmptyDocks = stationNumberOfEmptyDocks;
+		numberOfDocks = stationNumberOfDocks;
 	}
 
 	public int getStationIdentity() {
-		return StationIdentity;
+		return id;
 	}
 
 	public String getStationName() {
-		return StationName;
+		return name;
 	}
 
 	public long getStationTerminalName() {
-		return StationTerminalName;
+		return terminalName;
 	}
 
 	public float getStationLatitude() {
-		return StationLatitude;
+		return latitude;
 	}
 
 	public float getStationLongitude() {
-		return StationLongitude;
+		return longitude;
 	}
 
 	public boolean isStationInstalled() {
-		return StationInstalled;
+		return installed;
 	}
 
 	public boolean isStationLocked() {
-		return StationLocked;
+		return locked;
 	}
 
 	public long getStationInstallDate() {
-		return StationInstallDate;
+		return installDate;
 	}
 
 	public long getStationRemovalDate() {
-		return StationRemovalDate;
+		return removalDate;
 	}
 
 	public boolean isStationTemporaryStatus() {
-		return StationTemporaryStatus;
+		return temporaryStatus;
 	}
 
 	public int getStationNumberOfBikes() {
-		return StationNumberOfBikes;
+		return numberOfBikes;
 	}
 
 	public int getStationNumberOfEmptyDocks() {
-		return StationNumberOfEmptyDocks;
+		return numberOfEmptyDocks;
 	}
 
 	public int getStationNumberOfDocks() {
-		return StationNumberOfDocks;
+		return numberOfDocks;
 	}
 
 }
