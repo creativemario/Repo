7f66eb11094e901ff9d6c8e77a01b8cd86678354
CreativeMario <thomasmather7@hotmail.co.uk>
2015-10-25 11:31:11 +0000

    Commiting main

---
+++ SportCommanderSimulator/src/main/java/uk/co/reformtheempire/sportcommandersimulator/main/SportCommanderSimulatorStandalone.java
@@ -0,0 +1,9 @@
+package uk.co.reformtheempire.sportcommandersimulator.main;
+
+public class SportCommanderSimulatorStandalone {
+
+	public static void main(String[] args){
+		System.out.println("Hello World");
+	}
+	
+}
